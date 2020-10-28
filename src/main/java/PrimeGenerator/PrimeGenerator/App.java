cdce6c3f65242eb90d100b2c9d8615662ff449ff
Federico Durante <dufe@live.it>
2020-10-28 11:42:00 +0100

    first commit

---
+++ src/main/java/PrimeGenerator/PrimeGenerator/App.java
@@ -0,0 +1,14 @@
+package PrimeGenerator.PrimeGenerator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        MyMath math = new MyMath();
+        System.out.println(math.primeNumbersGenerator(20));
+    }
+}
